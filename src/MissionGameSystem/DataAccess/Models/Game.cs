c3f942e5b9d895a1b13e955ab599190ffef5bbe5
Kye Norman-Gill <kye.normangill@tafensw.net.au>
2016-10-22 21:53:01 +1100

    Initial commit

---
+++ src/MissionGameSystem/DataAccess/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionGameSystem.DataAccess.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Theme { get; set; }
+        //make linking table for missions
+        //make linking table for contestants
+        //make linking table for prize
+        public DateTime StartTime { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
